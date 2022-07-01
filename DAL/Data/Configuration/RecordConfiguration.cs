68df4269158906f52741a8f1f056868a3a189295
Foldner <newfoldner37@gmail.com>
2022-07-01 22:20:46 +0300

    Добавил начальные данные БД для миграций

--- DAL/Data/Configuration/RecordConfiguration.cs
+++ DAL/Data/Configuration/RecordConfiguration.cs
@@ -7,26 +7,26 @@ namespace DAL.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Record> builder)
         {
-        builder.ToTable("record");
+            builder.ToTable("record");
 
-        builder.HasIndex(e => e.MenuId, "menu_idx");
+            builder.HasIndex(e => e.MenuId, "menu_idx");
 
-        builder.HasIndex(e => e.UserId, "user_idx");
+            builder.HasIndex(e => e.UserId, "user_idx");
 
-        builder.Property(e => e.Id).ValueGeneratedNever();
+            builder.Property(e => e.Id).ValueGeneratedNever();
 
-        builder.Property(e => e.Price).HasPrecision(4, 2);
+            builder.Property(e => e.Price).HasPrecision(4, 2);
 
-        builder.HasOne(d => d.Menu)
-            .WithMany(p => p.Records)
-            .HasForeignKey(d => d.MenuId)
-            .OnDelete(DeleteBehavior.ClientSetNull)
-            .HasConstraintName("menuFK");
+            builder.HasOne(d => d.Menu)
+                .WithMany(p => p.Records)
+                .HasForeignKey(d => d.MenuId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("menuFK");
 
-        builder.HasOne(d => d.User)
-            .WithMany(p => p.Records)
-            .HasForeignKey(d => d.UserId)
-            .HasConstraintName("userFK");
+            builder.HasOne(d => d.User)
+                .WithMany(p => p.Records)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("userFK");
         }
     }
 }

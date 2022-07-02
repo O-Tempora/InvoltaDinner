2d2994223394c525d033bdb39808c2c220d45440
Mikhail Panfilov~ <mixalpalf@gmail.com>
2022-07-02 18:34:38 +0300

    feat: авторизация JWT-токенами

--- Dinner/Controllers/AccountController.cs
+++ Dinner/Controllers/AccountController.cs
@@ -1,111 +1,80 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Identity;
-// using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BLL.Models;
+using BLL.Interfaces;
+using DAL.Data;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
-// namespace Dinner.Controllers
-// {
-//     [Produces("application/json")]
-//     public class AccountController : Controller
-//     {
-//         private readonly ILogger<AccountController> _logger;
-//         private readonly UserManager<User> _userManager;
-//         private readonly SignInManager<User> _signInManager;
+namespace Dinner.Controllers
+{
+    [Produces("application/json")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IDbCrud _iDbCrud;
+        public AccountController(IDbCrud iDbCrud)
+        {      
+            _iDbCrud = iDbCrud;
+        }
 
-//         public AccountController(ILogger<AccountController> logger, UserManager<User> userManager, SignInManager<User> signInManager)
-//         {
-//             _logger = logger;
-//             _userManager = userManager;
-//             _signInManager = signInManager;
-//         }
+        [HttpPost]
+        [Route("api/account/signup")]
+        public async Task<IActionResult> SignUp([FromBody] User user)
+        {
+            
 
-//         [HttpPost]
-//         [Route("api/account/signup")]
-//         public async Task<IActionResult> SignUp([FromBody] SignUpModel model)
-//         {
-//             if (ModelState.IsValid)
-//             {
-//                 User user = new User
-//                 {
-//                     Email = model.Email,
-//                     Password = model.Password!,
-//                     UserName = model.UserName
-//                 };
+            return Ok();
+        }
 
-//                 var result = await _userManager.CreateAsync(user, model.Password);
-//                 if (result.Succeeded)
-//                 {
-//                     await _userManager.AddToRoleAsync(user, "User");
-//                     await _signInManager.SignInAsync(user, false);
-//                     var msg = new
-//                     {
-//                         message = "Добавлен новый пользователь: " + user.Email
-//                     };
-//                     return Ok(msg);
-//                 }
-//                 else
-//                 {
-//                     foreach (var error in result.Errors)
-//                     {
-//                         ModelState.AddModelError(string.Empty, error.Description);
-//                     }
-//                     var errorMsg = new
-//                     {
-//                         message = "Ошибка добавления пользователя",
-//                         error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
-//                     };
-//                     return Ok(errorMsg);
-//                 }
-//             }
-//             else
-//             {
-//                 var errorMsg = new
-//                 {
-//                     message = "Неверные входные данные",
-//                     error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
-//                 };
-//                 return Ok(errorMsg);
-//             }
-//         }
+        [HttpPost]
+        [Route("api/account/signin")]
+        public async Task<IActionResult> SignIn([FromBody] SignInModel model)
+        {
+            var user = AuthenticateUser(model.Email, model.Password);
 
-//         [HttpPost]
-//         [Route("api/account/signin")]
-//         public async Task<IActionResult> SignIn([FromBody] SignInModel model)
-//         {
-//             if (ModelState.IsValid)
-//             {
-//                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
-//                 if (result.Succeeded)
-//                 {
-//                     var msg = new
-//                     {
-//                         message = "Вход успешно выполнен"
-//                     };
-//                     return Ok(msg);
-//                 }
-//                 else
-//                 {
-//                     ModelState.AddModelError("", "Неверный логин или пароль");
-//                     var errorMsg = new
-//                     {
-//                         message = "Вход не выполнен",
-//                         error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
-//                     };
-//                     return BadRequest(errorMsg);
-//                 }
-//             }
-//             else
-//             {
-//                 var errorMsg = new
-//                 {
-//                     message = "Вход не выполнен",
-//                     error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
-//                 };
-//                 return BadRequest(errorMsg);
-//             }
-//         }
-//     }
-// }
+            if (user != null)
+            {
+                //Генерация токена
+                var token = GenerateJWT(user);
+                return Ok(new 
+                {
+                    access_token = token
+                });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+        
+        private User AuthenticateUser (string email, string password)
+        {
+            return _iDbCrud.GetUserByEmailAndPassword(email, password); //Поменять
+        }
+
+        private string GenerateJWT (User user)
+        {
+            var securityKey  = AuthOptions.GetSymmetricSecurityKey();
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>() {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+            };
+            claims.Add(new Claim("role", user.Role));
+
+            var token = new JwtSecurityToken( AuthOptions.ISSUER,
+                AuthOptions.AUDIENCE,
+                claims,
+                expires: DateTime.Now.AddSeconds(AuthOptions.LIFETIME),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}

02cdbb368f6d030258c49a4f74d4010d98f17787
Mikhail Panfilov~ <mixalpalf@gmail.com>
2022-07-09 21:39:30 +0300

    feat: Добавил автоматический вызов метода, удаляющего меню за прошедший месяц и создающего новое в 00:00:00 1-го числа каждого месяца

---
+++ Dinner/BackgroundTasks/MenuTask.cs
@@ -0,0 +1,37 @@
+using BLL.Interfaces;
+using BLL.Models;
+
+namespace Dinner.BackgroundTasks 
+{
+    public class MenuTask : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private PeriodicTimer _timer = new (GetTimeUntilNextMonth());
+        public MenuTask(IServiceScopeFactory scopeFactory)
+        {      
+            _scopeFactory = scopeFactory;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            while (await _timer.WaitForNextTickAsync(stoppingToken)
+                && !stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var scopedService = scope.ServiceProvider.GetRequiredService<IDbCrud>();
+                    scopedService.UpdateMonthMenu();
+                }
+                _timer = new PeriodicTimer(GetTimeUntilNextMonth());
+            }
+        }
+
+        public static TimeSpan GetTimeUntilNextMonth()
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime desiredTime = new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month, 1, 0, 0, 0);
+            desiredTime.AddMonths(1);
+            TimeSpan timeDifference = desiredTime - currentTime;
+            return timeDifference;
+        }
+    }
+}

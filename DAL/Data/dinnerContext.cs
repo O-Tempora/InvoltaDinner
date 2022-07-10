6cbd22343918f048d73ac7e73a061e291b67c96e
Mikhail Panfilov~ <mixalpalf@gmail.com>
2022-07-10 13:26:43 +0300

    feat: Добавил валидацию даты при попытке создания/редактирования записи

--- DAL/Data/dinnerContext.cs
+++ DAL/Data/dinnerContext.cs
@@ -29,7 +29,7 @@ namespace DAL.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySql("server=database;user=root;password=1944Rommel1944;database=dinner;", 
+                optionsBuilder.UseMySql("server=localhost;user=root;password=plshelp;database=dinner;", 
                 Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"), 
                 options => options.EnableRetryOnFailure(3));
             }
